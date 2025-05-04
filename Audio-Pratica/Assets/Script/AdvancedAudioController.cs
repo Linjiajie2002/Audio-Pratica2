bd80f0f761032b209dfcb17b997f3d8e570209fd
Linjiajie2002 <92087408+Linjiajie2002@users.noreply.github.com>
2025-05-04 15:38:35 +0200

    JJL-SnapShots

---
+++ Audio-Pratica/Assets/Script/AdvancedAudioController.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using UnityEngine.Audio;
+using System.Collections;
+
+public class AdvancedAudioController : MonoBehaviour
+{
+    [Header("Audio Mixer设置")]
+    [SerializeField] AudioMixer audioMixer;
+    [SerializeField] AudioMixerSnapshot indoorSnapshot;
+    [SerializeField] AudioMixerSnapshot outdoorSnapshot;
+    
+    [Header("过渡设置")]
+    [SerializeField] float fadeDuration = 2f;
+    [SerializeField] float transitionSpeed = 1f;
+
+    private bool isOutdoor = false;
+    private float currentLerp;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isOutdoor = !isOutdoor;
+            StopAllCoroutines();
+            StartCoroutine(TransitionEnvironment());
+        }
+    }
+
+    IEnumerator TransitionEnvironment()
+    {
+        float targetLerp = isOutdoor ? 1 : 0;
+        
+        while (Mathf.Abs(currentLerp - targetLerp) > 0.01f)
+        {
+            currentLerp = Mathf.MoveTowards(
+                currentLerp, 
+                targetLerp, 
+                transitionSpeed * Time.deltaTime
+            );
+
+            // 混合两个快照
+            audioMixer.TransitionToSnapshots(
+                new[] { indoorSnapshot, outdoorSnapshot },
+                new[] { 1 - currentLerp, currentLerp },
+                fadeDuration
+            );
+
+            yield return null;
+        }
+    }
+
+    // 在编辑器中可视化调试
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawWireCube(transform.position, GetComponent<BoxCollider>().size);
+    }
+}
